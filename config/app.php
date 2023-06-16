67d90ab878cbdbf4523d512dca6733991c2b2717
GarfieldFan <57429414+GarfieldFan@users.noreply.github.com>
2023-06-16 15:15:44 +1000

    Update app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://code-test-jiahui-fan-095f3e79ce0f.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
